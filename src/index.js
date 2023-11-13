1c3892d36a9135fa1a25776cea92caf6b4ae7ae3
liya <liyarheon@gmail.com>
2023-11-13 17:21:15 +0300

    test function app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import "./style.scss";
-import "bootstrap/dist/css/bootstrap.css";
+import './style.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'on-change';
+import render from './view';
 
-console.log("Hello World!");
+const app = () => {
+  const state = {
+    uiFormState: {
+      valid: true,
+      state: 'filling',
+      url: '',
+    },
+  };
+  const watchedState = onchange(state, render);
+  const form = document.querySelector('.rss-form');
+  form.addEventListener('input', (e) => {
+    watchedState.uiFormState.url = e.target.value;
+  });
+};
+app();
