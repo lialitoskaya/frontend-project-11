75cc2c13b434676e19eb0fd7456b1b70ad99e997
liya <liyarheon@gmail.com>
2023-12-20 19:38:11 +0300

    added timer to track request timeout errors

--- src/util/contentRequest.js
+++ src/util/contentRequest.js
@@ -1,13 +1,21 @@
-const contentRequest = (url) => fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`, {
-  cache: 'no-store',
-})
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Network response was not ok.');
-  })
-  .catch(() => {
+const contentRequest = (url) => {
+  const promise = new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      reject(new Error('timeout of 10000ms exceeded'));
+    }, 10000);
+    fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`, {
+      cache: 'no-store',
+    }).then((response) => {
+      clearTimeout(timeoutId);
+      if (response.ok) {
+        resolve(response.json());
+        return;
+      }
+      throw new Error('Network response was not ok.');
+    });
+  }).catch(() => {
     throw new Error('networkError');
   });
+  return promise;
+};
 export default contentRequest;
